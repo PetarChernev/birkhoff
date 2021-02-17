d870e003b55137257a0f347aafb77dbc3c425275
Petar <petar.chernev@movedigital.ch>
2021-02-17 21:22:06 +0200

    Refactored algorithm in separate file

---
+++ algorithm.py
@@ -0,0 +1,71 @@
+import math
+import numpy as np
+
+from incidence_matrix import IncidenceMatrix
+from knots import Knots
+from polynomial import Polynomial
+
+
+def solve_lagrangian(x, w, incidence_matrix):
+    assert len(x.shape) == 1
+    assert incidence_matrix.array[:, 1:].sum() == 0
+    x = x[incidence_matrix.array[:, 0].astype(bool)]
+    if isinstance(w, np.ndarray):
+        w = Knots(w, incidence_matrix)
+
+    denominator_matrix = np.array([x ** i for i in range(len(x) - 1, -1, -1)])
+    denominator = np.linalg.det(denominator_matrix)
+
+    numerator_matrix = np.vstack([w.long, denominator_matrix])
+    coefficients = - np.array([(-1) ** (i + 2) * np.linalg.det(np.delete(numerator_matrix, obj=i, axis=0))
+                               for i in range(len(x), 0, -1)])
+    coefficients /= denominator
+
+    return Polynomial(coefficients)
+
+
+def solve(x, w, incidence_matrix) -> Polynomial:
+    if isinstance(w, np.ndarray):
+        w = Knots(w, incidence_matrix)
+    if isinstance(incidence_matrix, np.ndarray):
+        incidence_matrix = IncidenceMatrix(incidence_matrix)
+    if not incidence_matrix.polya_condition():
+        print("Warning: incidence_matrix does not satisfy Polya condition and it is not balanced. The problem "
+              "migth not have a solution for the specified x and w.")
+    assert len(x) == incidence_matrix.k
+
+    if incidence_matrix.is_lagrangian:
+        return solve_lagrangian(x, w, incidence_matrix)
+
+    if incidence_matrix.is_irreducible:
+        up, down = incidence_matrix.reduce()
+
+        up_knots = Knots(w[:, :-1], up)
+        up_solution = solve(x, up_knots, up)
+
+        down_knots = Knots(w[:, :-1], down)
+        down_solution = solve(x, down_knots, down)
+
+        x_pow_n_derivatives = np.array([math.factorial(incidence_matrix.n) / math.factorial(incidence_matrix.n - j) * x ** (incidence_matrix.n - j)
+                                        for j in range(incidence_matrix.n)]).T
+
+        derivatives_knots_up = Knots(x_pow_n_derivatives, up)
+        up_n_solution = solve(x, derivatives_knots_up, up)
+
+        derivatives_knots_down = Knots(x_pow_n_derivatives, down)
+        down_n_solution = solve(x, derivatives_knots_down, down)
+
+        c = (up_solution[1] - down_solution[1]) / (up_n_solution[1] - down_n_solution[1])
+
+        return up_solution - (incidence_matrix.p_n - up_n_solution) * c
+    else:
+        left, right = incidence_matrix.decompose()
+
+        right_g_solution = solve(x, Knots(w[:, left.n + 1:], right), right)
+        right_solution = right_g_solution.integrate(left.n + 1)
+
+        left_w = Knots(w[:, :left.n + 1] -
+                       np.array([right_solution.differentiate(i)(x) for i in range(left.n + 1)]).T,
+                       left)
+        left_solution = solve(x, left_w, left)
+        return left_solution + right_solution
--- incidence_matrix.py
+++ incidence_matrix.py
@@ -5,7 +5,6 @@ from typing import Tuple
 import numpy as np
 
 from polynomial import Polynomial
-from knots import Knots
 
 
 class IncidenceMatrix:
@@ -21,12 +20,9 @@ class IncidenceMatrix:
 
         self.p_n = Polynomial([0] * (self.n - 1) + [1])
 
-    @staticmethod
-    def create(array: np.ndarray):
-        try:
-            return LagrangianIncidenceMatrix(array)
-        except AssertionError:
-            return IncidenceMatrix(array)
+    @cached_property
+    def is_lagrangian(self) -> bool:
+        return self.array[:, 1:].sum() == 0
 
     @cached_property
     def polya_condition(self):
@@ -40,7 +36,7 @@ class IncidenceMatrix:
         for i in range(1, self.n + 1):
             arrays = np.hsplit(self.array, [i])
             try:
-                left, right = [IncidenceMatrix.create(a) for a in arrays]
+                left, right = [IncidenceMatrix(a) for a in arrays]
             except AssertionError:
                 continue
             if left.is_irreducible:
@@ -52,74 +48,7 @@ class IncidenceMatrix:
         up[0, np.nonzero(up[0, :])[0][-1]] = 0
         down = np.copy(self.array[:, :-1])
         down[-1, np.nonzero(down[-1, :])[0][-1]] = 0
-        return IncidenceMatrix.create(up), IncidenceMatrix.create(down)
+        return IncidenceMatrix(up), IncidenceMatrix(down)
 
     def __repr__(self):
         return self.array.__repr__()
-
-    def solution(self, x, w) -> Polynomial:
-        assert len(x) == self.k
-        if isinstance(w, np.ndarray):
-            w = Knots(w, self)
-
-        if self.is_irreducible:
-            up, down = self.reduce()
-
-            up_knots = Knots(w[:, :-1], up)
-            up_solution = up.solution(x, up_knots)
-
-            down_knots = Knots(w[:, :-1], down)
-            down_solution = down.solution(x, down_knots)
-
-            x_pow_n_derivatives = np.array([math.factorial(self.n)/math.factorial(self.n - j) * x ** (self.n - j)
-                                            for j in range(self.n)]).T
-
-            derivatives_knots_up = Knots(x_pow_n_derivatives, up)
-            up_n_solution = up.solution(x, derivatives_knots_up)
-
-            derivatives_knots_down = Knots(x_pow_n_derivatives, down)
-            down_n_solution = down.solution(x, derivatives_knots_down)
-
-            c = (up_solution[1] - down_solution[1]) / (up_n_solution[1] - down_n_solution[1])
-
-            return up_solution - (self.p_n - up_n_solution) * c
-        else:
-            left, right = self.decompose()
-
-            right_g_solution = right.solution(x, Knots(w[:, left.n + 1:], right))
-            right_solution = right_g_solution.integrate(left.n + 1)
-
-            left_w = Knots(w[:, :left.n + 1] -
-                                np.array([right_solution.differentiate(i)(x) for i in range(left.n + 1)]).T,
-                           left)
-            left_solution = left.solution(x, left_w)
-            return left_solution + right_solution
-
-
-class LagrangianIncidenceMatrix(IncidenceMatrix):
-    def __init__(self, array: np.ndarray):
-        assert array[:, 1:].sum() == 0
-        super().__init__(array)
-        self.first_col = array[:, 0]
-
-    def solution(self, x, w):
-        assert len(x.shape) == 1
-        x = x[self.first_col.astype(bool)]
-        if isinstance(w, np.ndarray):
-            w = Knots(w, self)
-
-        denominator_matrix = np.array([x**i for i in range(len(x)-1, -1, -1)])
-        denominator = np.linalg.det(denominator_matrix)
-
-        numerator_matrix = np.vstack([w.long, denominator_matrix])
-        coefficients = - np.array([(-1) ** (i + 2) * np.linalg.det(np.delete(numerator_matrix, obj=i, axis=0))
-                                   for i in range(len(x), 0, -1)])
-        coefficients /= denominator
-
-        return Polynomial(coefficients)
-
-
-array = np.array([[1,0, 0], [0,0, 0], [1,0, 0], [0,0,1]])
-m = IncidenceMatrix.create(array)
-p = m.solution(np.array([1,2,3,4]), np.array([4,5, 14]))
-print()
